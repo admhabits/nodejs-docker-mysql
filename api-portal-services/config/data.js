bdb978f6d6d6631589bdadbe8167d095c357d169
alam.santiko <alamhafidz61@gmail.com>
2021-10-14 17:18:06 +0700

    update

--- api-portal-services/config/data.js
+++ api-portal-services/config/data.js
@@ -1,15 +1,15 @@
-module.exports = {
-    HOST:process.env.DB_HOST,
-    DB_NAME:process.env.DB_NAME,
-    USER_NAME:process.env.DB_USER,
-    DB_SECRET:process.env.DB_PASSWORD,
-    PORT: process.env.DB_PORT,
-}
-
 // module.exports = {
-//     HOST: 'localhost',
-//     DB_NAME:'portal',
-//     USER_NAME:'root',
-//     DB_SECRET:'keren121224',
-//     PORT: 4000,
+//     HOST:process.env.DB_HOST,
+//     DB_NAME:process.env.DB_NAME,
+//     USER_NAME:process.env.DB_USER,
+//     DB_SECRET:process.env.DB_PASSWORD,
+//     PORT: process.env.DB_PORT,
 // }
+
+module.exports = {
+    HOST: '10.106.12.196',
+    DB_NAME:'portal',
+    USER_NAME:'root',
+    DB_SECRET:'zWTbwHJ2bs',
+    PORT: 4000,
+}
