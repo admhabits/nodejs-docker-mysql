623bd253ee6063dd9dc991a1091669cbb4a27f66
alam.santiko <alamhafidz61@gmail.com>
2021-10-27 18:51:57 +0700

    some update

---
+++ application/Dockerfile
@@ -0,0 +1,14 @@
+FROM node:14
+
+RUN mkdir /app
+
+ADD . /app
+
+WORKDIR /app
+
+RUN npm install
+
+CMD npm start
+
+EXPOSE 4000
+
---
+++ application/config/data.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//     HOST:process.env.DB_HOST,
+//     DB_NAME:process.env.DB_NAME,
+//     USER_NAME:process.env.DB_USER,
+//     DB_SECRET:process.env.DB_PASSWORD,
+//     PORT: process.env.DB_PORT,
+// }
+
+module.exports = {
+    HOST: 'localhost',
+    DB_NAME:'api_portal',
+    USER_NAME:'root',
+    DB_SECRET:'keren121224',
+    PORT: 4000,
+}
