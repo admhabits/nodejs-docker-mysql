423a6b82e71aedb0f03c0c9767f3d802af9c4596
alam.santiko <alamhafidz61@gmail.com>
2021-11-11 12:42:08 +0700

    update

--- config/data.js
+++
@@ -1,9 +0,0 @@
-// require('dotenv').config(); 
-/* Uncomment Line 1 Ketika menggunakan env pengembangan local */
-module.exports = {
-    HOST: 'mysqldb',
-    DB_NAME:'api_portal',
-    USER_NAME:'appnode',
-    DB_SECRET:'P@ssw0rd2345',
-    PORT: 4000,
-}
