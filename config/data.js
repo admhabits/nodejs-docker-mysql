bb549c5c644d7b8e6502118664c0b02048826f18
alam.santiko <alamhafidz61@gmail.com>
2021-11-11 11:27:54 +0700

    prepare merge

---
+++ config/data.js
@@ -0,0 +1,9 @@
+// require('dotenv').config(); 
+/* Uncomment Line 1 Ketika menggunakan env pengembangan local */
+module.exports = {
+    HOST: 'mysqldb',
+    DB_NAME:'api_portal',
+    USER_NAME:'appnode',
+    DB_SECRET:'P@ssw0rd2345',
+    PORT: 4000,
+}
